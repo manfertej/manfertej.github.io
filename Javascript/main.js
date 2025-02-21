37da75dc6ba5bc5faf404a3997bc29c1ad7524c2
Alfonso Fernández <58088960+manfertej@users.noreply.github.com>
2025-02-21 03:16:38 +0100

    Remaking with Bootstrap
    Replace as much CSS as possible with Bootstrap, making it responsive.

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,16 +1,7 @@
-let mybutton = document.getElementById("goToTop");
-window.onscroll = function() {scrollFunction()};
-
-
-function scrollFunction() {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+function goToTop() {
   document.documentElement.scrollTop = 0; 
 } 
+
+function goTo(id) {
+  document.getElementById(id).scrollIntoView(true);
+}
